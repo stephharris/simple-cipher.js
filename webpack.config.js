6a59c13141f4651e1c3f60b2e7c3f64448c92409
Steph Harris <stephanie.harris@firstlook.org>
2021-01-03 17:57:17 -0500

    updating webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-      {
         test: /\.ts$/,
         exclude: /(node_modules)/,
         use: 'ts-loader',
